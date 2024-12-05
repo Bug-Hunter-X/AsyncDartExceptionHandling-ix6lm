f67af0c903a56e381266baf05e7319332f1e7400
BugHunter-X <askzenai@gmail.com>
2024-12-05 13:26:07 +0100

    Add bug.dart file

---
+++ bug.dart
@@ -0,0 +1,25 @@
+```dart
+import 'dart:async';
+
+Future<void> main() async {
+  final completer = Completer<int>();
+
+  // Simulate an asynchronous operation that might fail
+  Future.delayed(Duration(seconds: 2), () {
+    // This operation can either succeed or fail randomly
+    final shouldFail = false; // Change this to true to simulate failure
+    if (shouldFail) {
+      completer.completeError(Exception('Operation failed'));
+    } else {
+      completer.complete(42);
+    }
+  });
+
+  try {
+    final result = await completer.future;
+    print('Result: $result');
+  } catch (e) {
+    print('Error: $e');
+  }
+}
+```
